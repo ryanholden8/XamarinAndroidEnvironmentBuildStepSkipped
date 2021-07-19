56e21ee433b1f1c568c89240c1a1a972673a3f58
Ryan Holden <rholden@bethel.jw.org>
2021-07-19 14:06:36 -0400

    Initial commit

---
+++ AndroidEnvironment/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using AndroidX.AppCompat.App;
+
+namespace AndroidEnvironment
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
